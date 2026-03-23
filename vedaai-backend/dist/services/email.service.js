7a399636d55bf09fce8326e1c17cc9928bd19927
Tech-Shreyansh <shreyanshagarwal.16.2022@gmail.com>
2026-03-23 22:05:35 +0530

    enhance : add error logging

--- vedaai-backend/dist/services/email.service.js
+++ vedaai-backend/dist/services/email.service.js
@@ -15,10 +15,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendOtpEmail = void 0;
 const nodemailer_1 = __importDefault(require("nodemailer"));
 const transporter = nodemailer_1.default.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false,
     },
 });
 const sendOtpEmail = (email, otp) => __awaiter(void 0, void 0, void 0, function* () {
